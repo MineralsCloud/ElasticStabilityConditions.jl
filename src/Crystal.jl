1fff476ce46aaacd27a56b550d45d77f4b9c3f8a
Qi Zhang <singularitti@outlook.com>
2018-07-25 04:13:23 -0400

    Add Crystal.jl

---
+++ src/Crystal.jl
@@ -0,0 +1,22 @@
+"""
+# module Crystal.jl
+
+- Julia version: 0.7
+- Author: qz
+- Date: 2018-07-25
+
+# Examples
+
+```jldoctest
+julia>
+```
+"""
+module Crystal
+
+struct CrystalSystem
+    elastic_matrix::Base.Matrix
+end
+
+
+
+end

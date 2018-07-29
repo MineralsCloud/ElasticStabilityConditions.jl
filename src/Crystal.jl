111f637f40cec9058723be6febe68a6de2a44776
Qi Zhang <singularitti@outlook.com>
2018-07-29 16:26:40 -0400

    Simplify Crystal.jl

--- src/Crystal.jl
+++ src/Crystal.jl
@@ -1,5 +1,5 @@
 """
-# module Crystal.jl
+# module Crystal
 
 - Julia version: 0.7
 - Author: qz
@@ -15,13 +15,9 @@ module Crystal
 
 struct CrystalSystem
     elastic_matrix::Matrix
-    CrystalSystem(elastic_matrix) = size(elastic_matrix) == (2, 2) ? new(elastic_matrix) : error("Size should be 6x6!")
+    CrystalSystem(elastic_matrix) = size(elastic_matrix) == (6, 6) ? new(elastic_matrix) : error("Size should be 6x6!")
 end
 
-function compliance_matrix(m::CrystalSystem)
-    return inv(m.elastic_matrix)
-end
-
-println(compliance_matrix(CrystalSystem([1 2;3 4])))
+compliance_matrix(m::CrystalSystem) = inv(m.elastic_matrix)
 
 end

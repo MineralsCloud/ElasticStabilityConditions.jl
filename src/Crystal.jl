403fb794c7d53dac7f1cda2f7ff1c1b0c2b36f6a
Qi Zhang <singularitti@outlook.com>
2018-07-29 15:30:56 -0400

    fix: LoadError in Crystal.jl

--- src/Crystal.jl
+++ src/Crystal.jl
@@ -15,12 +15,13 @@ module Crystal
 
 struct CrystalSystem
     elastic_matrix::Matrix
+    CrystalSystem(elastic_matrix) = size(elastic_matrix) == (2, 2) ? new(elastic_matrix) : error("Size should be 6x6!")
 end
 
 function compliance_matrix(m::CrystalSystem)
-    return inv(m)
+    return inv(m.elastic_matrix)
 end
 
-
+println(compliance_matrix(CrystalSystem([1 2;3 4])))
 
 end

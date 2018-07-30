46e459da5cda3bbde463f173f8edede233eadc86
Qi Zhang <singularitti@outlook.com>
2018-07-30 00:02:56 -0400

    Add rules in `stability_conditions`

--- src/Crystal.jl
+++ src/Crystal.jl
@@ -18,6 +18,20 @@ struct CrystalSystem
     CrystalSystem(elastic_matrix) = size(elastic_matrix) == (6, 6) ? new(elastic_matrix) : error("Size should be 6x6!")
 end
 
-compliance_matrix(m::CrystalSystem) = inv(m.elastic_matrix)
+compliance_matrix(c::CrystalSystem) = inv(c.elastic_matrix)
+
+stability_conditions(c::CrystalSystem) = "No stability conditions defined for `$(typeof(c))!`"
+stability_conditions(c::CubicSystem) = [
+    "C_{11} > | C_{12} |",
+    "C_{11} + 2 C_{12} > 0",
+    "C_{44} > 0"
+]
+stability_conditions(c::HexagonalSystem) = [
+    "C_{11} > | C_{12} |",
+    "2 C_{13}^2 < C_{33} (C_{11} + C_{12})",
+    "C_{44} > 0",
+    "C_{66} > 0"
+]
+
 
 end
